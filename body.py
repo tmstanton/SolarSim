7661057039e92df89c0696ec9aeece1f5f69f425
Thomas Stanton <hetmstanton@gmail.com>
2023-02-09 16:47:13 +0000

    added run, implemented paramter file usage

--- body.py
+++ body.py
@@ -1,22 +1,31 @@
-# imports
+ # imports
 import numpy as np
 
 # class
-class Body(object):
+class Planet(object):
 
     # -=-=-=- Initialisation methods -=-=-=-
 
-    def __init__(self:object, name:str, mass:float, orbit:float, colour:str) -> None:
+    def __init__(self:object, name:str, mass:float, radius:float, orbit:float, period:float, colour:str) -> None:
         """ Initialise a celestial body given certain parameters """
         self.name = name
-        self.mass = mass
+        if 'msun' not in mass:
+            self.mass = float(mass)
+        else:
+            self.mass = 1 * float(mass.strip('msun'))
         self.col  = colour.strip()
         # set initial position
+        self.rorbit = float(orbit)
         self.position = np.array([orbit, 0.0])
 
-    def InitialiseVelocity(self:object, v:float) -> None:
+    def InitialiseVelocity(self:object, v:list) -> None:
         """ Initialse object with given velocity """
         self.velocity = v
+
+    @staticmethod
+    def Generate(row:tuple) -> object:
+        # disect row tuple, adjust mass units
+        return Planet(*row)
 
     # -=-=-=- Euler-Cromer Update Methods -=-=-=- 
 
@@ -32,7 +41,6 @@ class Body(object):
 
     # -=-=-=- Beeman Update Methods -=-=-=-
     
-
     def KineticEnergy(self:object) -> float:
         """ Calculates and returns kinetic energy in Joules """
         return (np.dot(self.velocity, self.velocity)) * self.mass / 2
--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import astropy.constants as constants
+from body import Planet
 
 # simulation class
 class Simulation(object):
@@ -9,21 +10,40 @@ class Simulation(object):
     def __init__(self:object, path:str) -> None:
         
         # read in input file
+        data = np.genfromtxt(path, delimiter='', dtype=str, skip_header=1)
         # set up array of objects
+        self.planets = np.array([Planet.Generate(row) for row in data], dtype=object)
+        # TODO: worth adding some idea of visualisation rather than radius 
         # set initial velocities
         # set constants
+        self.SetConstants()
         # initialise simulation
-        pass
+        self.InitialiseVelocities()
+
+        for p in self.planets:
+            print(p.name)
 
     def SetConstants(self:object) -> None:
         self.Msun = 3
         self.Mearth = 3
-        self.G = constants.G # 1.18638e-4 AU^3 / Msun / yr^2
+        self.G = 1.18638e-4 # AU^3 / Msun / yr^2
         self.dt = 0.01 # or 300? not sure
         self.niter = 100000
 
     def InitialiseVelocities(self:object) -> None:
-        pass
+        """ initialises the velocities of all objects, assuming that the sun 
+            starts off the simulation with zero velocity """
+        for p, planet in enumerate(self.planets):
+            # assuming sun stays stationary / no 
+            if p == 0:
+                planet.InitialiseVelocity(np.zeros(2, dtype=float))
+            else:
+                mag_r = np.linalg.norm(planet.rorbit)
+                tan_vel = np.sqrt(self.G * self.planets[0].mass / mag_r)
+                planet.InitialiseVelocity(np.array([0., tan_vel]))
+
+            print(planet.velocity)
+
 
     def TotalKE(self:object) -> float:
         """ Determines the kinetic energies of all the objects in the sim """
